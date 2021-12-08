26c5e0a7fbe94b0a606d413d166990d819e5d2dc
IvoObr <ivo_obr@hotmail.com>
2021-12-08 13:25:44 +0200

    rmlogs-interactive

--- rmlogs.js
+++ rmlogs.js
@@ -1,44 +1,74 @@
 const fs = require('fs');
 const path = require('path');
-
-/**
- * Recursively deletes files from directory matching RegEx
- * @param dir - directory
- * @param filter - filenames
- * @param excludeFolders - folders to exclude
- * @param fileList - files paths to be deleted
- */
-
-function rmlogs(props) {
-
-    const { dir, filter, fileList = [],
-        ignore = /(node_modules|dist|src|.git|.vscode)/ } = props;
-    
-    const files = fs.readdirSync(dir);
-    
-    files.forEach((file) => {
-        const filePath = path.join(dir, file);
-        const excluded = filePath.match(ignore);
-
-        if (!excluded) {
-            const fileStat = fs.lstatSync(filePath);
-
-            if (fileStat.isDirectory()) {
-                rmlogs({ dir: filePath, filter, ignore, fileList });
-
-            } else if (filter.test(filePath)) {
-                fileList.push(filePath);
-                console.log('deleting: ', filePath);
-                fs.unlink(filePath, (error) => error && console.error(error));
-            }
-        }
+const readline = require('readline')
+    .createInterface({
+        input: process.stdin,
+        output: process.stdout
     });
 
-    return fileList;
-}
+(function() {
+    /**
+     * Recursively finds files from directory matching RegEx
+     * @param dir - directory
+     * @param filter - filenames
+     * @param excludeFolders - folders to exclude
+     * @param fileList - file paths to be deleted
+     */
+    function findFilesR(props) {
 
-rmlogs({
-    dir: path.join(__dirname, './'), // start from current dir
-    filter: new RegExp(/.log\b/),
-    fileList: []
-});
+        const { dir, filter, fileList = [],
+            ignore = /(node_modules|dist|src|.git|.vscode)/ } = props;
+    
+        const files = fs.readdirSync(dir);
+    
+        files.forEach((file) => {
+            const filePath = path.join(dir, file);
+            const excluded = filePath.match(ignore);
+
+            if (!excluded) {
+                const fileStat = fs.lstatSync(filePath);
+
+                if (fileStat.isDirectory()) {
+                    findFilesR({ dir: filePath, filter, ignore, fileList });
+
+                } else if (filter.test(filePath)) {
+                    fileList.push(filePath);
+                }
+            }
+        });
+
+        return fileList;
+    }
+
+    function deleteFiles(fileList) {
+        let ask = `\u001B[33m\u001B[1mAre you sure that you want to delete these files? (yes/no)\u001B[0m\n`;
+        fileList.forEach(filePath => ask += filePath + '\n');
+
+        readline.question(ask + '\n', answer => {
+            console.log();
+
+            if (answer !== 'yes') {
+                console.log('\u001B[32m\u001B[1mAborting\u001B[0m');
+                readline.close();
+                return;
+            }
+
+            fileList.forEach(function(filePath) {
+                fs.unlink(filePath, function(error) {
+                    if (error) console.error(error);
+                    else console.log('\u001B[31m\u001B[1mDeleted:\u001B[0m ', filePath);    
+                });     
+            });
+
+            readline.close();
+        });
+    }
+
+    const fileList = findFilesR({
+        dir: path.join(__dirname, './'), // start from current dir
+        filter: new RegExp(/.log\b/),
+        fileList: []
+    });
+
+    deleteFiles(fileList);
+})();
