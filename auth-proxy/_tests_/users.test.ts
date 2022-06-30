ac6f30b7d4674c6ef6ace5dd5207152848a50fff
IvoObr <ivo_obr@hotmail.com>
2022-06-30 13:12:28 +0300

    401-test-update

--- auth-proxy/_tests_/users.test.ts
+++ auth-proxy/_tests_/users.test.ts
@@ -64,7 +64,7 @@ describe.skip('OK 200 <Users api tests>', (): void => {
     
 });
 
-describe('Bad Request 400 <Users api tests>', (): void => {
+describe.skip('Bad Request 400 <Users api tests>', (): void => {
     
     it('400   Name       POST     /api/v1/auth/pub/register',
         async function() { await UsersTest.register('', UserRole.Admin, 'Password123@', `${genBase36Key(8)}@yopmail.com`, StatusCodes.BAD_REQUEST); });
@@ -99,7 +99,7 @@ describe('Bad Request 400 <Users api tests>', (): void => {
     it('400   Email      PUT      /api/v1/user/update/:id',
         async function() { await UsersTest.updateUser('IvoG', 'Password123@', INCORRECT_EMAIL, StatusCodes.BAD_REQUEST); });
 
-    it('404   Email      POST     /api/v1/auth/pub/send-reset-password',
+    it('403   Email      POST     /api/v1/auth/pub/send-reset-password',
         async function() { await UsersTest.sendResetPass(INCORRECT_EMAIL, StatusCodes.FORBIDDEN); });
 
     it('400   Password   POST     /api/v1/auth/reset-password',
@@ -110,19 +110,22 @@ describe('Bad Request 400 <Users api tests>', (): void => {
 
 });
 
-describe.skip('Unauthorized 401 <Users api tests>', (): void => {
+describe('Unauthorized 401 <Users api tests>', (): void => {
 
     it('201   *         POST     /api/v1/auth/pub/register',
         async function() { await UsersTest.register(); });
 
-    // todo: confirmRegistration
-    // todo: sendConfirmRegistration
-   
-    it('401   *         POST     /api/v1/auth/pub/login',
+    it('201   *          POST    /api/v1/auth/confirm-registration',
+        async function() { await UsersTest.confirmRegistration(); });
+
+    it('201   *          POST    /api/v1/auth/pub/send-confirm-registration',
+        async function() { await UsersTest.sendConfirmRegistration(UsersTest.config.email); });
+    
+    it('401   Password  POST     /api/v1/auth/pub/login',
         async function() { await UsersTest.login(UsersTest.config.email, INCORRECT_PASS, StatusCodes.UNAUTHORIZED); });
     
-    it('401   Email     POST     /api/v1/auth/pub/login',
-        async function() { await UsersTest.login(INCORRECT_EMAIL, UsersTest.config.password, StatusCodes.UNAUTHORIZED); });
+    it('403   Email     POST     /api/v1/auth/pub/login',
+        async function() { await UsersTest.login(INCORRECT_EMAIL, UsersTest.config.password, StatusCodes.FORBIDDEN); });
   
     it('401   *         GET      /api/v1/api/user/all',
         async function() { await UsersTest.getAllUsers(false, StatusCodes.UNAUTHORIZED); });
